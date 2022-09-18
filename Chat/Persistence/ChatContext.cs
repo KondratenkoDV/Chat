e0fb164f50bb502db9d4062f7325355003df6775
Denis <Denis@DESKTOP-601R0L5>
2022-09-18 11:13:19 +0300

    Added class ChatContext and configurations

---
+++ Chat/Persistence/ChatContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Configurations;
+using System;
+
+namespace Persistence
+{
+    public class ChatContext : DbContext
+    {
+        public DbSet<Сomment> Сomments { get; set; } = null!;
+
+        public ChatContext(DbContextOptions<ChatContext> options)
+            : base(options)
+        {
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new СommentConfiguration());
+        }
+    }
+}
---
+++ Chat/Persistence/Configurations/СommentConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Persistence.Configurations
+{
+    internal class СommentConfiguration : IEntityTypeConfiguration<Сomment>
+    {
+        public void Configure(EntityTypeBuilder<Сomment> builder)
+        {
+            builder
+                .HasKey(c => c.Id);
+
+            builder
+                .Property(c => c.UserName)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder
+                .Property(c => c.Email)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder
+                .Property(c => c.HomePage)
+                .HasMaxLength(255);
+
+            builder
+                .Property(c => c.Text)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder
+                .Property(c => c.Ip)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder
+                .Property(c => c.BrowserData)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder
+                .Property(c => c.DateAdded)
+                .IsRequired();
+        }
+    }
+}
