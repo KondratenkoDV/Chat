4aba4841c43d32716ed9c6e0a2a0e51eecc1568e
Denis <Denis@DESKTOP-601R0L5>
2022-09-18 10:39:16 +0300

    Added class Сomment

---
+++ Chat/Domain/Сomment.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Сomment
+    {
+        public int Id { get; }
+
+        public string UserName { get; }
+
+        public string Email { get; }
+
+        public string HomePage { get; }
+
+        public string Text { get; }
+
+        public string Ip { get; }
+
+        public string BrowserData { get; }
+
+        public DateTime DateAdded { get; }
+
+        public Сomment(
+            string userName,
+            string email,
+            string homePage,
+            string text,
+            string ip,
+            string browserData)
+        {
+            if (string.IsNullOrEmpty(userName))
+            {
+                throw new ArgumentException(nameof(userName));
+            }
+
+            if (string.IsNullOrEmpty(email))
+            {
+                throw new ArgumentException(nameof(email));
+            }
+
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new ArgumentException(nameof(text));
+            }
+
+            if (string.IsNullOrEmpty(ip))
+            {
+                throw new ArgumentException(nameof(ip));
+            }
+
+            if (string.IsNullOrEmpty(browserData))
+            {
+                throw new ArgumentException(nameof(browserData));
+            }
+
+            UserName = userName;
+            Email = email;
+            HomePage = homePage;
+            Text = text;
+            Ip = ip;
+            BrowserData = browserData;
+
+            DateAdded = DateTime.Now;
+        }
+    }
+}
