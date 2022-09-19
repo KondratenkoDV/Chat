8c5967692736b5ffb07146e3e69fbdeb2835c81a
Denis <Denis@DESKTOP-601R0L5>
2022-09-19 19:11:18 +0300

    Added QueryPaginationDto

---
+++ Chat/API/DTOs/Comment/QueryPaginationDto.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common;
+
+namespace API.DTOs.Comment
+{
+    public class QueryPaginationDto : PaginationParameters
+    {
+        public bool UserName { get; set; } = false;
+
+        public bool Email { get; set; } = false;
+
+        public bool SortDown { get; set; } = true;
+    }
+}
