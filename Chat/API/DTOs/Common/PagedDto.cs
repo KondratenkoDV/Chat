aae1e52ed4c60dd63df08c127f3f8d1734b53366
Denis <Denis@DESKTOP-601R0L5>
2022-09-19 19:11:37 +0300

    Added PagedDto

---
+++ Chat/API/DTOs/Common/PagedDto.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Common;
+
+namespace API.DTOs.Common
+{
+    public class PagedDto<T>
+    {
+        public IEnumerable<T>? Entities { get; set; }
+
+        public PaginationMetadata? PaginationMetadata { get; set; }
+    }
+}
