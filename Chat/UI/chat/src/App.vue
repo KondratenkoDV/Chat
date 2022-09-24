7841e90761c89ba76c5e85b458bab1c8408a8fbe
Denis <Denis@DESKTOP-601R0L5>
2022-09-24 19:57:49 +0300

    Added UI

---
+++ Chat/UI/chat/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <v-app id="inspire">
+      <div class="text-center">
+        <v-btn class="mx-2" fab dark small color="primary">
+            <v-icon>
+              mdi-minus
+            </v-icon>
+        </v-btn>
+      </div>
+    </v-app>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
---
+++ Chat/UI/chat/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import vuetify from './plugins/vuetify'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
