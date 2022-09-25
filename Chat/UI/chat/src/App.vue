f9921dadc66526a0868d85c31782653a528de251
Denis <Denis@DESKTOP-601R0L5>
2022-09-25 20:23:31 +0300

    Added CAPTCHA

--- Chat/UI/chat/src/App.vue
+++ Chat/UI/chat/src/App.vue
@@ -1,42 +1,11 @@
 <template>
   <v-app>
-    <v-app-bar color="deep-purple accent-4" dense dark app>
-      <v-toolbar-title>Chat</v-toolbar-title>      
-        <AddNewComment />
-      <template v-if="$vuetify.breakpoint.mdAndUp">
-              <v-spacer></v-spacer>
-
-      <v-spacer></v-spacer>
-      <v-select
-                v-model="sortBy"
-                flat
-                solo-inverted
-                hide-details
-                :items="keys"
-                prepend-inner-icon="mdi-magnify"
-                label="Sort by"
-                class="ps-16"
-              ></v-select>
-      <v-btn-toggle v-model="sortDesc" mandatory>
-        <v-btn depressed color="deep-purple accent-4" :value="false">
-          <v-icon>mdi-arrow-up</v-icon>
-        </v-btn>
-        <v-btn depressed color="deep-purple accent-4" :value="true">
-          <v-icon>mdi-arrow-down</v-icon>
-        </v-btn>
-      </v-btn-toggle>
-      </template>  
-    </v-app-bar>
+    <AppToolBar/>
     <v-content>
       <v-container>
-        {{message}}
-
         <div>
-          {{parentComments}}
-        </div>
-
-        
-        
+          <CommentView/>
+        </div>        
       </v-container>
       
     </v-content>
@@ -46,32 +15,12 @@
 
 
 <script>
-import {variables} from "./api";
-import AddNewComment from "./components/AddNewComment.vue";
+import CommentView from "./components/CommentView.vue";
+import AppToolBar from "./components/ToolBar.vue";
 export default {
   components: {
-    AddNewComment
-  },
-  data(){
-    return {
-      sortDesc: false,
-      sortBy: 'name',
-      parentComments: [],
-    }
-  },
-  methods: {
-    getParentComments() {
-      this.axios.get(variables.API_URL + "Comment")
-      .then((response)=>{
-        this.parentComments=response.data;
-      });
-    }
-  },
-  method: function(){
-    this.getParentComments();
-  },
-  created(){
-    this.getParentComments();
-  }
+    CommentView,
+    AppToolBar
+  },  
 }
 </script>
---
+++ Chat/UI/chat/src/components/CommentView.vue
@@ -0,0 +1,60 @@
+<template>
+    <div app>
+        <tr v-for="com in parentComments.entities" :key="com.Id">
+            <td></td>
+        </tr>
+    </div>
+</template>
+
+<script>
+import {variables} from "../api";
+
+export default {
+    data(){
+    return {
+        parentComments: {
+            entities: [],
+            paginationMetadata: {
+                
+            }
+        },
+    }
+  },
+  methods: {
+    getParentComments() {
+      this.axios.get(variables.API_URL + "Comment")
+      .then((response)=>{
+        this.parentComments=response.data;
+      });
+    }
+  },
+  created(){
+    this.getParentComments();
+  }
+}
+</script>
+
+<style scoped>
+p {
+    text-align: justify;
+    text-indent: 30px;
+    font-family: Serif;
+    font-size: 14px;
+    }
+h1, h2, h3 {
+    color: blue; /* тоже самое, что и #0000FF */
+}
+table, img {border: none;}
+li a {text-decoration: none;}
+div, th, td {
+    padding-left: 10px;
+    padding-right: 10px;
+    background-color: yellow;
+}
+a {color: black; font-weight: bold;}
+p a, li a {font-weight: normal; text-decoration: none;}
+p a:hover, li a:hover {
+    color: #00FF00; text-decoration: underline;
+    }
+    
+</style>
